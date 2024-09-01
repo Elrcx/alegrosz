1ff7d9fbf5ae165a4a37afa3ec5ba187aacc4126
elrcx <eliarcx@gmail.com>
2024-09-01 17:08:11 +0200

    setup django templates with basic configuration

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def contact(request):
-    return HttpResponse('<h1>Tu jest kontakt</h1>')
+    return render(request, 'contact/contact.html')
