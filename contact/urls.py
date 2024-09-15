c20419712198b2ae9ce06f25505c3eb311d20509
elrcx <eliarcx@gmail.com>
2024-09-15 17:04:55 +0200

    forms, generic views, crispy forms

--- contact/urls.py
+++ contact/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
+from .views import ContactView
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('contact/', views.contact, name='contact'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
 
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 
-# Create your views here.
-def contact(request):
-    return render(request, 'contact/contact.html')
+from . import forms
+
+
+class ContactView(FormView):
+    template_name = 'contact/contact.html'
+    form_class = forms.ContactForm
+    success_url = reverse_lazy('home:home')
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+
+        return super().form_valid(form)
